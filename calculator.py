c0c820f6cd894495e9140f4a4196de5f8f197273
Nayl Real Repe <nickrealrepe43995@gmail.com>
2026-06-10 01:24:20 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,29 @@
-Initial upload of calculator project
+# Python Simple Calculator
+
+print("=== Simple Calculator ===")
+
+num1 = float(input("Enter 1st number: "))
+operator = input("Choose an operator (+, -, *, /): ")
+num2 = float(input("Enter 2nd number: "))
+
+if operator == "+":
+    result = num1 + num2
+    print("Result:", result)
+
+elif operator == "-":
+    result = num1 - num2
+    print("Result:", result)
+
+elif operator == "*":
+    result = num1 * num2
+    print("Result:", result)
+
+elif operator == "/":
+    if num2 == 0:
+        print("Error: Cannot divide by zero!")
+    else:
+        result = num1 / num2
+        print("Result:", result)
+
+else:
+    print("Invalid operator!")
